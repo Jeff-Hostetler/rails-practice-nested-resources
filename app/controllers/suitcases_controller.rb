74202fbdbdfd30354e5a71c23cbf8704ac72ea44
Jeff Hostetler <hostetjm@gmail.com>
2014-11-17 11:45:09 -0700

    h

---
+++ app/controllers/suitcases_controller.rb
@@ -0,0 +1,53 @@
+class SuitcasesController < ApplicationController
+
+  before_action do
+    @passenger = Passenger.find(params[:passenger_id])
+  end
+
+  def index
+    @suitcases = @passenger.suitcases
+  end
+
+  def show
+    @suitcase = @passenger.suitcases.find(params[:id])
+  end
+
+  def new
+    @suitcase = @passenger.suitcases.new
+  end
+
+  def create
+    @suitcase = @passenger.suitcases.new(suitcase_params)
+    if @suitcase.save
+      redirect_to passenger_suitcase_path(@passenger, @suitcase)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @suitcase = @passenger.suitcases.find(params[:id])
+  end
+
+  def update
+    @suitcase = @passenger.suitcases.find(params[:id])
+    if @suitcase.update(suitcase_params)
+      redirect_to passenger_suitcase_path(@passenger, @suitcase)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @suitcase = @passenger.suitcases.find(params[:id])
+    @suitcase.destroy
+    redirect_to suitcases_path
+  end
+
+  private
+
+  def suitcase_params
+    params.require(:suitcase).permit(:description)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: "passengers#index"
+  
+  resources :passengers do
+    resources :suitcases
+  end
+end
